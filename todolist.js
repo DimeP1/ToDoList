d0c25af7dc6b77c2e8e167acfbe2b9c9272a2e42
DimeP1 <71714575+DimeP1@users.noreply.github.com>
2020-12-12 01:24:33 -0500

    add

--- todolist.js
+++ todolist.js
@@ -1,3 +1,4 @@
+let string = []
 document.querySelector("#Input").addEventListener("submit", response)
 
 let newInput = document.getElementById("new input");
@@ -8,20 +9,41 @@ function response(event){
     event.preventDefault();
     
     let input= newInput.value;
-    let string = []
+   
     string.push(input);
     const list = string.map(thelist => 
-        `<ol id="demo">${thelist} <button onclick="responseToClick()">x</button><button id="check" onclick="responseToClickTwo()">check</button>
+        `<ol id="demo">${thelist} <button class= "delete" onclick="responseToClick()">x</button><button class="check" onclick="responseToClickTwo()">check</button>
         </ol>`);
         listOfItems.innerHTML=list.join("");
 }
 
+//function responseToClick() {
+//	///alert('hello');
+//	let myobj = document.getElementById("demo");
+//	myobj.remove();
+//}
+
+//function responseToClickTwo(style) {
+//	let myobj = document.getElementById("demo").style.textDecoration = "line-through";
+//}
 function responseToClick() {
 	///alert('hello');
-	let myobj = document.getElementById("demo");
-	myobj.remove();
+	///let myobj = document.getElementById("demo");
+	//myobj.remove();
+if(event.target.classList.contains('delete')){
+	event.target.parentElement.remove();
+}
 }
 
-function responseToClickTwo(style) {
-	let myobj = document.getElementById("demo").style.textDecoration = "line-through";
+///function responseToClickTwo(style) {
+///	let myobj = document.getElementById("demo")
+	
+///}
+function responseToClickTwo() {
+	///alert('hello');
+	///let myobj = document.getElementById("demo");
+	//myobj.remove();
+if(event.target.classList.contains('check')){
+	event.target.parentElement.style.textDecoration = "line-through";
+}
 }
